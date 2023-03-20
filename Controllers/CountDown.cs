9eda9565f31cac6743fd46dc68891f6d26e0e530
Alex Junior <alexjr2oficial@gmail.com>
2023-03-20 18:22:01 -0300

    creating class that will be responsible for calculating the time

---
+++ Controllers/CountDown.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stopwatch_With_Csharp.Controllers
+{
+    public class CountDown
+    {
+        public static void StartDefault() { }
+
+        public static void StartPerson() { }
+    }
+}
