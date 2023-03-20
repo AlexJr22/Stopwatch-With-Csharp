916e82ec1a2256b541098ddc8accb0b71a887423
Alex Junior <alexjr2oficial@gmail.com>
2023-03-20 18:17:03 -0300

    Creating the console app starter files

---
+++ Controllers/Menu.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Stopwatch_With_Csharp.Controllers
+{
+    public class Menu
+    {
+        public static void Show()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Stopwatch_With_Csharp.Controllers;
+
+namespace Stopwatch_With_Csharp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Menu.Show();
+        }
+    }
+}
