abb9242dccecef684c93eadbee9270b8dcb636cb
Alex Junior <alexjr2oficial@gmail.com>
2023-03-21 18:36:04 -0300

    refactoring CountDown.cs and creating the 'CalculateTime()' method

--- Controllers/CountDown.cs
+++ Controllers/CountDown.cs
@@ -4,9 +4,8 @@ namespace Stopwatch_With_Csharp.Controllers
 {
     public class CountDown
     {
-        public static void StartDefault()
+        private static void CalculateTime()
         {
-            Console.Clear();
             while (true)
             {
                 DateTime now = DateTime.Now;
@@ -29,6 +28,37 @@ namespace Stopwatch_With_Csharp.Controllers
             }
         }
 
+        private static void CalculateTime(int days, int month, int year)
+        {
+            while (true)
+            {
+                DateTime now = DateTime.Now;
+                DateTime nextYear = new DateTime(year, month, days);
+
+                TimeSpan Diff = nextYear - now;
+                long totalSeconds = (long)Diff.TotalSeconds;
+
+                int RestSeconds = (int)totalSeconds % 60;
+
+                int Minutes = (int)totalSeconds / 60;
+                int RestMinutes = Minutes % 60;
+
+                int Hours = (int)Minutes / 60;
+                int RestHours = Hours % 24;
+
+                int Days = (int)Hours / 24;
+
+                HandleTimer(Days, RestHours, RestMinutes, RestSeconds);
+            }
+        }
+
+        public static void StartDefault()
+        {
+            Console.Clear();
+
+            CalculateTime();
+        }
+
         private static void HandleTimer(int days, int hours, int minutes, int secnds)
         {
             Console.Clear();
@@ -44,7 +74,49 @@ namespace Stopwatch_With_Csharp.Controllers
         public static void StartPerson()
         {
             Console.Clear();
-            Console.WriteLine("StopWacht Person");
+
+            Console.WriteLine("*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*");
+            for (var lines = 0; lines < 7; lines++)
+                Console.WriteLine("|                                 |");
+            Console.WriteLine("*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*");
+
+            Console.SetCursorPosition(9, 1);
+            Console.Write("DIGITE UMA DATA");
+            Console.SetCursorPosition(1, 2);
+            Console.WriteLine("=================================");
+
+            Console.SetCursorPosition(2, 4);
+            Console.Write("DIA: ");
+            short PersonDay = Convert.ToInt16(Console.ReadLine());
+
+            Console.SetCursorPosition(2, 5);
+            Console.Write("MÊS: ");
+            short PersonMonth = Convert.ToInt16(Console.ReadLine());
+
+            Console.SetCursorPosition(2, 6);
+            Console.Write("ANO: ");
+            short PersonYear = Convert.ToInt16(Console.ReadLine());
+
+            CalculateTimePerson(PersonDay, PersonMonth, PersonYear);
+        }
+
+        private static void CalculateTimePerson(int days, int month, int year)
+        {
+            Console.Clear();
+            if (year < DateTime.Now.Year)
+                year = (short)DateTime.Now.Year;
+
+            if (month > 12)
+                month = 12;
+
+            if (month < 1)
+                month = 1;
+
+            int DaysInMonth = DateTime.DaysInMonth(year, month);
+            if (days > DaysInMonth)
+                days = DaysInMonth;
+
+            CalculateTime(days, month, year);
         }
     }
 }
